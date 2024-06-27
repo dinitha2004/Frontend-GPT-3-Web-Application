a7f7a488fce7aed8c2e239fa239ee012606b3623
dinitha2004 <chalani.20211032@iit.ac.lk>
2024-06-27 06:39:01 -0700

    possibility part

--- src/containers/possibility/Possibilty.jsx
+++ src/containers/possibility/Possibilty.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import possibilityImage from '../../assets/possibility.png';
 import './possibility.css';
+
 
 const Possibility = () => {
   return (
-    <div>
-        Posibility
+    <div className='gpt3__possibility section__padding' id="possibility">
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>The possibilities are <br /> beyond your imagination</h1>
+        <p>By gaining early access, you'll have the opportunity to explore the capabilities of one of the most advanced AI language models available. Whether you're looking to enhance your business applications, create innovative content, or simply experiment with cutting-edge technology, GPT-3 offers a wide range of possibilities.</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
     </div>
   )
 }
